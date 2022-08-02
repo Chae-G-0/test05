b395e7bdf1f4dc3fc799fa0a1abf47220ff3ffec
채가 <103270067+Chae-G-0@users.noreply.github.com>
2022-08-03 01:20:52 +0900

    Add files via upload

--- js/test05.js
+++ js/test05.js
@@ -6,4 +6,23 @@ $(function () {
         pauseOnHover: false,
         pauseOnFocus: false,
     });
+
+    $('.lensSlide').slick({
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        slideToShow: 3,
+    });
+
+    $('.tabMenu>>li>a').on('click', function(e){
+        e.preventDefault();
+        var idx = $(this).parent().index();
+        $('.tabContent>li').removeClass('on');
+        $('.tabContent>li').eq(idx).addClass('on');
+        $('.tabMenu>li').removeClass('on');
+        $(this).parent().addClass('on');
+    });
+
 });
