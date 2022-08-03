a43afb202c1145cb26412d86e675c3c22fd62990
chae <corkdud@naver.com>
2022-08-03 18:13:35 +0900

    220803

--- js/test05.js
+++ js/test05.js
@@ -7,22 +7,37 @@ $(function () {
         pauseOnFocus: false,
     });
 
-    $('.lensSlide').slick({
+    $(".lensSlide").slick({
         autoplay: true,
         arrows: false,
         dots: false,
         pauseOnHover: false,
         pauseOnFocus: false,
+        centerMode: true,
+        centerPadding: "400px",
         slideToShow: 3,
     });
+    console.log("댄다!");
 
-    $('.tabMenu>>li>a').on('click', function(e){
+    $(".tabMenu>li>a").on("click", function (e) {
         e.preventDefault();
         var idx = $(this).parent().index();
-        $('.tabContent>li').removeClass('on');
-        $('.tabContent>li').eq(idx).addClass('on');
-        $('.tabMenu>li').removeClass('on');
-        $(this).parent().addClass('on');
+        $(".tabContent>li").removeClass("on");
+        $(".tabContent>li").eq(idx).addClass("on");
+        $(".tabMenu>li").removeClass("on");
+        $(this).parent().addClass("on");
     });
 
+    const scrollContainer = document.querySelector(".mainPic");
+
+    scrollContainer.addEventListener("wheel", (e) => {
+        console.log(scrollContainer.scrollLeft, e.deltaY);
+        if (e.deltaY == -100 && scrollContainer.scrollLeft == 0) {
+            console.log(e.deltaY == -100);
+        } else if (e.deltaY == 100 && scrollContainer.scrollLeft > 3000) {
+        } else {
+            e.preventDefault();
+            scrollContainer.scrollLeft += e.deltaY;
+        }
+    });
 });
